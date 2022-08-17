08dce9f88e56fe09913865ab5e98ac743cc5f095
Fartomy <furkii_tekdemir@hotmail.com>
2022-08-17 21:35:22 +0300

    ft_export_edited

--- minishell/src/built-in_commands/export.c
+++ minishell/src/built-in_commands/export.c
@@ -6,11 +6,40 @@
 /*   By: ftekdrmi <ftekdrmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 18:37:12 by ftekdrmi          #+#    #+#             */
-/*   Updated: 2022/08/17 16:20:27 by ftekdrmi         ###   ########.fr       */
+/*   Updated: 2022/08/17 20:46:56 by ftekdrmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void export_add_variable(char *var)
+{
+    int     i;
+    bool    equal_ctrl;
+
+    i = -1;
+    equal_ctrl = false;
+    while (var[++i])
+    {
+        if(var[i] == '=')
+            equal_ctrl = true;
+    }
+    if(equal_ctrl == true)
+    {
+        printf(":d");
+    }
+    else
+    {
+        int a;
+
+        a = 0;
+        while (data.export[a])
+            a++;
+        data.export[a] = ft_strdup(var);
+        data.export[a + 1] = 0;
+        export_sorter(data.export);
+    }
+}
 
 void    ft_export(char **parse)
 {
@@ -21,5 +50,19 @@ void    ft_export(char **parse)
     {
         while (data.export[i])
             printf("declare -x %s\n", data.env[i++]);
+    }
+    else if(ft_strcmp(parse[i], "export") == 0 && parse[i + 1])
+    {
+        i = 1;
+        while (parse[i])
+        {
+            if(parse[i][0] == '_' && (parse[i][1] == 0 || parse[i][1] == '='))
+                printf("");
+            else if((parse[i][0] == '_' && parse[i][1] != 0) || ft_isalpha(parse[i][0]))
+                export_add_variable(parse[i]);
+            else
+                printf("minishell: %s: `%c': not a valid identifier\n", parse[0], parse[i][0]);
+            i++;
+        }
     }
 }
