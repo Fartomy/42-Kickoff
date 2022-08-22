f8965cce0c0dbd4c5818ed02a7bb54a1ac35f675
Fartomy <furkii_tekdemir@hotmail.com>
2022-08-22 14:12:27 +0300

    ft_exit_OK

--- minishell/src/built-in_commands/exit.c
+++ minishell/src/built-in_commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: ftekdrmi <ftekdrmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 16:39:25 by ftekdrmi          #+#    #+#             */
-/*   Updated: 2022/08/21 17:11:39 by ftekdrmi         ###   ########.fr       */
+/*   Updated: 2022/08/22 14:09:27 by ftekdrmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,45 @@
 void	ft_exit(char **parse)
 {
 	printf("exit\n");
-	if(!parse[1])
+	
+	int i;
+	int x;
+
+	i = 1;
+	if(!parse[i])
 		exit(0);
-	else if(ft_isdigit(parse[1][0]) && !parse[2] && ft_isdigit(parse[1][0]) == 0)
-		exit(ft_atoi(parse[1]));
-	else if(ft_isdigit(parse[2][0]) && parse[2] && ft_isdigit(parse[1][0]) == 1)
+	else if(parse[i] && !parse[i + 1])
 	{
-		printf("minishell: exit: too many arguments");
-		exit(1);
+		while (parse[i])
+		{
+			x = -1;
+			while (parse[i][++x])
+			{
+				if(ft_isalpha(parse[i][x]))
+				{
+					printf("minishell: exit: %s: numeric argument required\n", parse[i]); 
+					exit(255);
+				}
+			}
+			exit(ft_atoi(parse[i]));
+		}
 	}
 	else
 	{
-		printf("minishell: exit: %s: numeric argument required", parse[1]);
-		exit(255);
+		while (parse[i])
+		{
+			x = -1;
+			while (parse[i][++x])
+			{
+				if(ft_isalpha(parse[i][x]))
+				{
+					printf("minishell: exit: %s: numeric argument required\n", parse[i]); 
+					exit(255);
+				}
+			}
+			printf("minishell: exit: too many arguments\n");
+			exit(1);
+		}
 	}
+	
 }
