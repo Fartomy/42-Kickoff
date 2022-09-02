5b7e384720dce1731e3b8d732b6cd9533142f447
Fartomy <furkii_tekdemir@hotmail.com>
2022-09-02 14:05:16 +0300

    alias_command_is_run

--- minishell/src/pipes/pipe_err_ctrl.c
+++ minishell/src/pipes/pipe_err_ctrl.c
@@ -6,7 +6,7 @@
 /*   By: ftekdrmi <ftekdrmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 01:41:19 by ftekdrmi          #+#    #+#             */
-/*   Updated: 2022/09/02 12:22:43 by ftekdrmi         ###   ########.fr       */
+/*   Updated: 2022/09/02 14:05:02 by ftekdrmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,29 @@ int pipe_err_ctrl(char **parse)
 {
 	int i;
 
-	i = -1;
+	i = 0;
 	if(ft_strcmp(parse[0], "|") == 0)
 	{
 		printf("minishell: syntax error near unexpected token `|'\n");
 		return (0);
 	}
-	while (parse[++i])
+	while (parse[i])
 	{
-		if(ft_strcmp(parse[i], parse[i + 1]) == 0)
+		if (ft_strcmp(parse[i], "|") == 0 && parse[i + 1] == 0)
 		{
 			printf("minishell: syntax error near unexpected token `|'\n");
 			return (0);
 		}
+		else if(ft_strcmp(parse[i], "|") == 0 && ft_strcmp(parse[i + 1], " ") == 0 && parse[i + 2] == 0)
+		{
+			printf("minishell: syntax error near unexpected token `|'\n");
+			return (0);	
+		}
+		else if(ft_strcmp(parse[i], "|") == 0 && ft_strcmp(parse[i + 1], " ") == 0 && ft_strcmp(parse[i + 2], "|") == 0)
+		{
+			printf("minishell: syntax error near unexpected token `|'\n");
+			return (0);
+		}
+		i++;
 	}
-	i = ft_arglen(parse);
-	printf("%s\n", parse[i - 1]);
-	return (1);
 }
