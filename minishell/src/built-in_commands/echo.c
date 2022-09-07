4b8566240d68ef712504668024119cb1a3ef8ecc
Fartomy <furkii_tekdemir@hotmail.com>
2022-09-07 04:22:45 +0300

    alias_command_is_run

--- minishell/src/built-in_commands/echo.c
+++ minishell/src/built-in_commands/echo.c
@@ -6,42 +6,48 @@
 /*   By: ftekdrmi <ftekdrmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 15:08:31 by ftekdrmi          #+#    #+#             */
-/*   Updated: 2022/09/03 19:04:22 by ftekdrmi         ###   ########.fr       */
+/*   Updated: 2022/09/07 04:21:19 by ftekdrmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    ft_echo(char **parse)
+static void	echo_if_n(char **parse, int *i)
 {
-	int i;
-	
+	while (parse[++(*i)])
+	{
+		printf("%s", parse[*i]);
+		if (parse[*i] && parse[*i + 1])
+			printf(" ");
+	}
+	data.status = 0;
+}
+
+static void	echo_if_not_n(char **parse, int *i)
+{
+	*i = 0;
+	while (parse[++(*i)])
+	{
+		printf("%s", parse[*i]);
+		if (parse[*i] && parse[*i + 1])
+			printf(" ");
+	}
+	printf("\n");
+	data.status = 0;
+}
+
+void	ft_echo(char **parse)
+{
+	int	i;
+
 	i = 1;
-	if(!parse[1])
+	if (!parse[1])
 	{
 		printf("\n");
 		data.status = 0;
 	}
-    else if ((ft_strcmp(parse[1], "-n")) == 0) // -n varsa
-	{
-		while (parse[++i])
-		{
-			printf("%s", parse[i]);
-			if (parse[i] && parse[i + 1])
-				printf(" ");
-		}
-		data.status = 0;
-	}
-	else if (ft_strcmp(parse[1], "-n")) // -n yoksa
-	{	
-		i = 0;
-		while (parse[++i])
-		{
-			printf("%s", parse[i]);
-			if (parse[i] && parse[i + 1])
-				printf(" ");
-		}
-		printf("\n");
-		data.status = 0;
-	}
+	else if (ft_strcmp(parse[1], "-n") == 0)
+		echo_if_n(parse, &i);
+	else if (ft_strcmp(parse[1], "-n"))
+		echo_if_not_n(parse, &i);
 }
