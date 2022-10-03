9d7767ba76d856038a34f4b45e318c6c6195d3cd
Fartomy <furkii_tekdemir@hotmail.com>
2022-10-04 02:14:01 +0300

    cpp_1_ex04_created

---
+++ cpp_modules/cpp_1/ex04/SedLosers.hpp
@@ -0,0 +1,52 @@
+#ifndef SEDLOSERS_HPP
+#define SEDLOSERS_HPP
+
+#include <iostream>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+
+class SedLosers
+{
+public:
+	void	setFilename(string fn)
+	{
+		fileName = fn;
+	}
+	void	setS1(string s)
+	{
+		s1 = s;
+	}
+	void	setS2(string s)
+	{
+		s2 = s;
+	}
+
+	string	getFilename(void)
+	{
+		return (fileName);
+	}
+	string getS1(void)
+	{
+		return (s1);
+	}
+	string getS2(void)
+	{
+		return (s2);
+	}
+
+private:
+	string fileName;
+	string s1;
+	string s2;
+	ofstream fot;
+	ifstream fin;
+};
+
+
+#endif
--- cpp_modules/cpp_1/ex04/main.cpp
+++ cpp_modules/cpp_1/ex04/main.cpp
@@ -5,28 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ftekdrmi <ftekdrmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/03 16:46:04 by ftekdrmi          #+#    #+#             */
-/*   Updated: 2022/10/03 16:46:05 by ftekdrmi         ###   ########.fr       */
+/*   Created: 2022/10/04 00:53:21 by ftekdrmi          #+#    #+#             */
+/*   Updated: 2022/10/04 00:53:22 by ftekdrmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Weapon.hpp"
-#include "HumanA.hpp"
-#include "HumanB.hpp"
+#include "SedLosers.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
-	Weapon club = Weapon("C-98");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("Elwerici");
-	bob.attack();
+	if(ac == 4)
+	{
+		SedLosers	fn;
 
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
-	club.setType("Famas Gold");
-	jim.attack();
+		fn.setFilename(char[1]);
+		fn.setS1(char[2]);
+		fn.setS2(char[3]);
+	}
+	else
+		cout << "Wrong Number of Arguments!" << endl;
 
 	return (0);
 }
