8d97aaae3686fbf4a18bdcf49f262be0b80de543
Furkan Tekdrmir <ftekdrmi@k1m08s02.42istanbul.com.tr>
2022-11-15 17:57:11 +0300

    cpp_5_ex01_maybe_finished

--- cpp_modules/cpp_5/ex01/main.cpp
+++ cpp_modules/cpp_5/ex01/main.cpp
@@ -15,18 +15,66 @@
 
 int main( void )
 {
-	Bureaucrat obj( "Tuncay", 60 );
 	try
-	{
-		Form frm( "Test", 64, 123 );
+	{	// We are trying brc grade bigger than frm sign grade. We will get an error. Because brc grade bigger than frm grade. Form's grade not enough of Tuncay's grade.
+		Bureaucrat brc( "Tuncay", 60 );
+		Form frm( "Test", 33, 123 );
 
 		cout << frm << endl;
-		obj.signForm(frm, obj);
+		brc.signForm( frm, brc );
 	}
 	catch ( exception &e )
 	{	
 		cerr << e.what() << endl;
 	}
+	cout << "/-----------------------------------------------------------------------/" << endl;
+	try
+	{
+		Bureaucrat brc2( "Ercan", 23 );
+		Form frm2( "Test-2", 45, 121);
 
-    return 0;
+		cout << frm2 << endl;
+		brc2.signForm( frm2, brc2 );
+	}
+	catch ( exception &e )
+	{
+		cerr << e.what() << endl;
+	}
+	cout << "/-----------------------------------------------------------------------/" << endl;
+	try
+	{
+		Form frm3( "Test-3", 156, 2 );
+	}
+	catch (exception &e)
+	{
+		cerr << e.what() << endl;
+	}
+	cout << "/-----------------------------------------------------------------------/" << endl;
+	try
+	{
+		Form frm4( "Test-4", 54, 234234 );
+	}
+	catch ( exception &e )
+	{
+		cerr << e.what() << endl;
+	}
+	cout << "/-----------------------------------------------------------------------/" << endl;
+	try
+	{
+		Form frm5( "Test-5", -52, 24 );
+	}
+	catch ( exception &e )
+	{
+		cerr << e.what() << endl;
+	}
+	cout << "/-----------------------------------------------------------------------/" << endl;
+	try
+	{
+		Form frm5( "Test-6", 125, -123 );
+	}
+	catch ( exception &e )
+	{
+		cerr << e.what() << endl;
+	}
+	return 0;
 }
