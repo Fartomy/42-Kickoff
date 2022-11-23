a9943ad93f54b9908559b09b06768072680bc846
Furkan Tekdrmir <ftekdrmi@k1m08s02.42istanbul.com.tr>
2022-11-23 16:33:30 +0300

    cpp_5_ex00_incGrade_and_decGrade_edited_it_is_reverse_means

--- cpp_modules/cpp_5/ex00/Bureaucrat.cpp
+++ cpp_modules/cpp_5/ex00/Bureaucrat.cpp
@@ -40,18 +40,18 @@ string Bureaucrat::getName( void ) const { return ( name ); }
 
 void Bureaucrat::decGrade( void )
 {
-    if ( grade - 1 < 1 )
+    if ( grade + 1 < 1 )
         throw ( GradeTooLowException() );
     else
-        grade--;
+        grade++;
 }
 
 void Bureaucrat::incGrade( void )
 {
-    if ( grade + 1 > 150 )
+    if ( grade - 1 > 150 )
         throw ( GradeTooHighException() );
     else
-        grade++;
+        grade--;
 }
 
 ostream& operator << ( ostream &ost, const Bureaucrat &obj )
