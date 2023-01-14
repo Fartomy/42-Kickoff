78cf3fbad771630875b42de9ee8551d6aeae44d4
Ali Furkan <ftekdrmi@k1m42s01.42istanbul.com.tr>
2023-01-14 23:57:22 +0300

    container

---
+++ ft_containers/inc/Vector.hpp
@@ -0,0 +1,17 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <iostream>
+
+namespace ft
+{
+template< class T, class Alloc = std::allocator<T> >
+class vector
+{
+public:
+
+};
+}
+
+
+#endif
