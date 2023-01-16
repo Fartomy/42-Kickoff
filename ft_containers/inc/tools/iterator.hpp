5aef5506e42a42ef4e58d7714cbb0563f5cba14b
Ali Furkan <ftekdrmi@k1m42s01.42istanbul.com.tr>
2023-01-16 18:46:09 +0300

    iterator edited

---
+++ ft_containers/inc/tools/iterator.hpp
@@ -0,0 +1,41 @@
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
+
+#include <iterator>
+#include <stddef.h>
+
+std::vector<>::iterator it;
+
+namespace ft
+{
+	template<typename Iter>	struct iterator_traits
+	{
+		typedef typename Iter::difference_type		difference_type;
+		typedef typename Iter::value_type			value_type;
+		typedef typename Iter::pointer				pointer;
+		typedef typename Iter::reference			reference;
+		typedef typename Iter::iterator_category	iterator_category;
+	};
+
+	template<typename T> struct iterator_traits<T*>
+	{
+		typedef std::ptrdiff_t						difference_type;
+		typedef T									value_type;
+		typedef T*									pointer;
+		typedef T&									reference;
+		typedef std::random_access_iterator_tag		iterator_category;
+	};
+
+	template<typename T> struct iterator_traits<const T*>
+	{
+		typedef std::ptrdiff_t						difference_type;
+		typedef T									value_type;
+		typedef const T*							pointer;
+		typedef const T&							reference;
+		typedef std::random_access_iterator_tag		iterator_category;
+	};
+}
+
+
+
+#endif
